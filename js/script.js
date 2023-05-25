ab7aa0048baaac7ae260c13f20ba56ea53494b87
davgo0103 <davvgo@gmail.com>
2023-05-25 16:25:14 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -148,22 +148,27 @@ const contractABI = [
 ];
 
 
-// 請修改以下合約位址
 async function getData() {
-    contractAddress = await document.getElementById("user-address").value;
-    // 請修改以下的提供者位址
-    provider = new Web3.providers.HttpProvider('http://localhost:7545');
-
-    // 建立 Web3 實例
-    web3 = await new Web3(provider);
-    // 建立合約實例
-    contract = await new web3.eth.Contract(contractABI, contractAddress);
     document.getElementById('goalAmount').textContent = "載入中..";
     document.getElementById('totalAmount').textContent = "載入中..";
     document.getElementById('numInvestors').textContent = "載入中..";
     document.getElementById('status').textContent = "載入中..";
+    contractAddress = await document.getElementById("user-address").value;
+    provider = new Web3.providers.HttpProvider('http://localhost:7545');
 
-    updateContractInfo()
+    // 建立 Web3 實例
+    web3 = await new Web3(provider);
+
+    // 驗證合約地址的有效性
+    if (!web3.utils.isAddress(contractAddress)) {
+        Swal.fire('Opps!', '無效的合約地址!!', 'error')
+        return;
+    }
+    // 建立合約實例
+    contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+
+    updateContractInfo();
 }
 
 
@@ -181,11 +186,11 @@ async function updateContractInfo() {
     const numInvestors = await contract.methods.numInvestors().call();
     var status = await contract.methods.status().call();
     const deadline = await contract.methods.deadline().call();
-    if(status == "Funding"){
+    if (status == "Funding") {
         status = "募款中.."
-    }else if(status == "Campaign Succeeded"){
+    } else if (status == "Campaign Succeeded") {
         status = "募款成功"
-    }else if(status == "Campaign Failed"){
+    } else if (status == "Campaign Failed") {
         status = "募款失敗"
     }
 
@@ -201,44 +206,44 @@ async function updateContractInfo() {
 function countdownTimer(deadline, elementId) {
     // 取得目標元素
     var element = document.getElementById(elementId);
-  
+
     function updateTimer() {
-      // 取得現在時間和截止時間的毫秒數
-      var currentTime = new Date().getTime();
-      var targetTime = new Date(deadline * 1000).getTime();
-  
-      // 計算剩餘時間（毫秒）
-      var remainingTime = targetTime - currentTime;
-  
-      // 計算剩餘天、小時、分鐘和秒數
-      var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
-  
-      // 更新元素內容為倒數計時器
-      if(contractAddress != ""){
-        element.textContent = days + "天 " + hours + "小時 " + minutes + "分鐘 " + seconds + "秒";
-      }else{
-        element.textContent = "等待輸入中..."
-      }
-      
-  
-      // 若還有剩餘時間，每秒更新一次
-      if (remainingTime > 0) {
-        setTimeout(updateTimer, 1000);
-      }else if(contractAddress != ""){
-        element.textContent = "募款活動已結束!"
-      }
+        // 取得現在時間和截止時間的毫秒數
+        var currentTime = new Date().getTime();
+        var targetTime = new Date(deadline * 1000).getTime();
+
+        // 計算剩餘時間（毫秒）
+        var remainingTime = targetTime - currentTime;
+
+        // 計算剩餘天、小時、分鐘和秒數
+        var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+        // 更新元素內容為倒數計時器
+        if (contractAddress != "") {
+            element.textContent = days + "天 " + hours + "小時 " + minutes + "分鐘 " + seconds + "秒";
+        } else {
+            element.textContent = "等待輸入中..."
+        }
+
+
+        // 若還有剩餘時間，每秒更新一次
+        if (remainingTime > 0) {
+            setTimeout(updateTimer, 1000);
+        } else if (contractAddress != "") {
+            element.textContent = "募款活動已結束!"
+        }
     }
-  
+
     // 呼叫函式開始倒數計時
     updateTimer();
-  }
-  
-  // 呼叫函式並指定截止時間和要更新的元素 ID
-  var elementId = "deadline";
-  countdownTimer(deadline, elementId);
+}
+
+// 呼叫函式並指定截止時間和要更新的元素 ID
+var elementId = "deadline";
+countdownTimer(deadline, elementId);
 
 
 async function fund() {
@@ -247,7 +252,7 @@ async function fund() {
     if (investmentAmount <= 0 && status == "Funding") {
         Swal.fire('Opps!', '投資金額必須大於零!!', 'error')
         return;
-    }else{
+    } else {
         Swal.fire('Opps!', '投資活動已結束!!', 'error')
     }
 
